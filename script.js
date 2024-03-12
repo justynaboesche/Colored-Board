6705c1988cb442c2d4473fc8f24249ec7d959792
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-12 11:42:23 +0100

    getting needed elements for js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Nic tu nie ma 😱')
+const box = document.querySelector('.box');
+const speedBtns = document.querySelectorAll('[data-settings="speed"');
+const colorBtns = document.querySelectorAll('[data-settings="color"');
+const slider = document.querySelector('#slider');
+const sliderInfo = document.querySelector('.slider-info');
+
+const squares = 546;
+let sliderValue = 70;
+let range = 360;
