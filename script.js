3a7fed25ad41baef0ff153c28e78f8ddf88470a0
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-12 22:31:34 +0100

    createSquare function created

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const sliderInfo = document.querySelector('.slider-info');
 const squares = 546;
 let sliderValue = 70;
 let range = 360;
+
+const createSquare = () => {
+	for (let i = 0; i < squares; i++) {
+		const square = document.createElement('div');
+		square.classList.add('square');
+
+		box.appendChild(square);
+	}
+};
+
+createSquare();
